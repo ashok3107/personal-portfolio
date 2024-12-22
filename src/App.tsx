4c5122f9db5ce89ec444add817f1e6015cc98c41
Ashok R <ashokmaddy1996@gmail.com>
2024-12-22 14:20:34 -0600

    removed browser router import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import SideNav from './components/SideNav/SideNav';
-import { Route, BrowserRouter, Routes, Navigate, HashRouter,  } from 'react-router-dom';
+import { Route, Routes, Navigate, HashRouter,  } from 'react-router-dom';
 
 import './App.css'
 import About from './components/About/About';
